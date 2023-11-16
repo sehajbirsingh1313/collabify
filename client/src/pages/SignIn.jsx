e284be10db1e5ea3ec2ee7dbe923687da5d3f4f8
Sehajbir Singh <Sehajbir Singh>
2023-11-16 14:37:31 +0530

    add redux persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { useDispatch, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [loading, error] = useSelector((state) => state.user)
+  const { loading, error } = useSelector((state) => state.user); // Destructure the properties
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -21,7 +21,7 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      dispatch (signInStart())
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -33,16 +33,16 @@ export default function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        dispatch(signInFailure().message)
+        dispatch(signInFailure(data.message)); // Pass the message to the action creator
         return;
       }
-      dispatch(signInSuccess().message)
+      dispatch(signInSuccess(data.message)); // Pass the message to the action creator
       navigate("/");
     } catch (error) {
-      dispatch(signInFailure().message)
-
+      dispatch(signInFailure(error.message));
     }
   };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1
@@ -75,7 +75,7 @@ export default function SignIn() {
       </form>
 
       <div className="flex gap-2 mt-5" style={{ color: "#000659" }}>
-        <p>Dont Have an account?</p>
+        <p>Don't have an account?</p>
         <Link to={"/sign-up"}>
           <span className="text-blue-700">Sign Up</span>
         </Link>
